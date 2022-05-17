6301f11c723d31993bc4210e740e250e3bb6808f
Adanelia <89346212+Adanelia@users.noreply.github.com>
2022-05-17 23:17:39 +0800

    Create format2.py

---
+++ format2.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*- 
+#!/usr/bin/env python 
+# https://blog.csdn.net/mocking_/article/details/106530420
+#import sys 
+#reload(sys) 
+#import importlib 
+#importlib.reload(sys) 
+# import imp 
+# import sys 
+# imp.reload(sys) 
+#sys.setdefaultencoding('utf8') 
+from gensim.corpora.wikicorpus import extract_pages,filter_wiki 
+import bz2file####记得安装呀！！！ 
+import re 
+from opencc import OpenCC####修改过 
+from tqdm import tqdm 
+import codecs 
+
+wiki = extract_pages(bz2file.open('./zhwiki-latest-pages-articles.xml.bz2')) 
+cc = OpenCC('t2s')###修改过 
+def wiki_replace(d): 
+    s = d[1] 
+    s = re.sub(':*{\|[\s\S]*?\|}', '', s) 
+    s = re.sub('<gallery>[\s\S]*?</gallery>', '', s) 
+    s = re.sub('(.){{([^{}\n]*?\|[^{}\n]*?)}}', '\\1[[\\2]]', s) 
+    s = filter_wiki(s) 
+    s = re.sub('\* *\n|\'{2,}', '', s) 
+    s = re.sub('\n+', '\n', s) 
+    s = re.sub('\n[:;]|\n +', '\n', s) 
+    s = re.sub('\n==', '\n\n==', s) 
+    s = u'【' + d[0] + u'】\n' + s 
+    return cc.convert(s).strip()####修改过 
+
+i = 0 
+f = codecs.open('wiki.txt', 'w', encoding='utf-8')####自己改名字就好了！叫狗蛋都行 
+w = tqdm(wiki, desc=u'已获取0篇文章') 
+for d in w: 
+    if not re.findall('^[a-zA-Z]+:', d[0]) and d[0] and not re.findall(u'^#', d[1]): 
+        s = wiki_replace(d) 
+        f.write(s+'\n\n\n') 
+        i += 1 
+        if i % 100 == 0: 
+        w.set_description(u'已获取%s篇文章'%i) 
+
+f.close()
